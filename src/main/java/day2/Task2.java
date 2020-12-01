668a00b329e38def7b611f68efdd272612c0734a
Kylechek <42637292+Kylechek@users.noreply.github.com>
2020-12-01 11:03:22 +0300

    [day2]: Add Task1-4 solution

--- src/main/java/day2/Task2.java
+++ src/main/java/day2/Task2.java
@@ -1,7 +1,17 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        for (int i = a; a <= b; i++) {
+            a = i + 1;
+            if (i % 5 == 0 && i % 10 != 0) {
+                System.out.print(i + " ");
+            }
+        }
     }
 }
--- src/main/java/day2/Task3.java
+++ src/main/java/day2/Task3.java
@@ -1,7 +1,17 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        while (a <= b) {
+            if (a % 5 == 0 && a % 10 != 0) {
+                System.out.print(a + " ");
+            }
+            a++;
+        }
     }
 }
