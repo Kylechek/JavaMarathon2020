a02564de88c14a2503e755afcb37cad01db05b9e
Ivan <ikonishuk@gmail.com>
2020-12-02 21:08:43 +0300

    [day3]: add Task1-3 solution

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,19 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        while (true) {
+            double numberOne = sc.nextDouble();
+            double numberTwo = sc.nextDouble();
+            double result = numberOne / numberTwo;
+            if (numberTwo == 0) {
+                break;
+            }
+            System.out.println(result);
+        }
     }
 }
