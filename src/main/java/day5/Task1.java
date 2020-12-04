37cb570a16d569acdc41647009e68a71904d2ba6
Ivan <ikonishuk@gmail.com>
2020-12-04 19:00:15 +0300

    [day5]: add Task1-2 solution

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,41 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car car = new Car();
+        car.setColor("Black");
+        car.setModel("BMW");
+        car.setStartYear(2010);
 
+        System.out.println(car.getStartYear() + car.getColor() + car.getModel());
+    }
+
+    static class Car {
+        private int StartYear;
+        private String Color;
+        private String Model;
+
+        public void setStartYear(int startYear) {
+            StartYear = startYear;
+        }
+
+        public void setColor(String color) {
+            Color = color;
+        }
+
+        public void setModel(String model) {
+            Model = model;
+        }
+
+        public int getStartYear() {
+            return StartYear;
+        }
+
+        public String getColor() {
+            return Color;
+        }
+
+        public String getModel() {
+            return Model;
+        }
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,32 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike car = new Motorbike(2015, "White", "Honda");
 
+        System.out.println(car.getStartYear() + car.getColor() + car.getModel());
+    }
+
+    static class Motorbike {
+        private final int startYear;
+        private final String color;
+        private final String model;
+
+        public Motorbike(int startYear, String color, String model) {
+            this.startYear = startYear;
+            this.color = color;
+            this.model = model;
+        }
+
+        public int getStartYear() {
+            return startYear;
+        }
+
+        public String getColor() {
+            return color;
+        }
+
+        public String getModel() {
+            return model;
+        }
     }
 }
