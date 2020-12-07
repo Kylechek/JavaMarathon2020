64278f7de3c0a893d01824b0e1a2c9f8a1b3e488
Ivan <ikonishuk@gmail.com>
2020-12-07 19:25:09 +0300

    [day8]: solution Task1-2

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,20 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
 
+        long time = System.currentTimeMillis();
+        for (int i = 0; i < 2000; i++) {
+            sb.append(i + " ");
+        }
+        System.out.println(sb);
+        System.out.println(System.currentTimeMillis() - time);
+        String s = "";
+        for (int i = 0; i < 2000; i++) {
+            s = s + i + " ";
+        }
+        System.out.println(sb);
+        System.out.println(System.currentTimeMillis() - time);
     }
+
 }
