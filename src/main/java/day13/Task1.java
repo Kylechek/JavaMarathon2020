570cc797b6e6f9af51cbf7b5c327e029722f98ec
Ivan <ikonishuk@gmail.com>
2020-12-13 20:43:15 +0300

    [day13]: solution Task

--- src/main/java/day13/Task1.java
+++ src/main/java/day13/Task1.java
@@ -2,6 +2,23 @@ package day13;
 
 public class Task1 {
     public static void main(String[] args) {
+        User userOne = new User("Ivan");
+        User userTwo = new User("Jon");
+        User userThree = new User("Bin");
 
+        userOne.sendMessage(userTwo, "Привет");
+        userOne.sendMessage(userTwo, "Как дела?");
+        userTwo.sendMessage(userOne, "Норм");
+        userTwo.sendMessage(userOne, "А у тебя");
+        userTwo.sendMessage(userOne, "?");
+        userThree.sendMessage(userOne, "Ты где?");
+        userThree.sendMessage(userOne, "Ты где?");
+        userThree.sendMessage(userOne, "Ты где?");
+        userOne.sendMessage(userThree, "Я тут");
+        userOne.sendMessage(userThree, "Я тут");
+        userOne.sendMessage(userThree, "Я тут");
+        userThree.sendMessage(userOne, "Позвони");
+
+        MessageDatabase.showDialog(userThree, userOne);
     }
 }
